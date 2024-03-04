adbc8186bf38064798d74a140db0b7f5a4b4ee8c
itzelsh <cchavez35@ucol.mx>
2024-03-04 13:11:13 -0600

    comando nltk

---
+++ toquenizador.py
@@ -0,0 +1,17 @@
+import nltk
+nltk.download('punkt')
+
+carpeta_nombre="Documentos\\"
+archivo_nombre="archivo_text.txt"
+
+with open(carpeta_nombre+archivo_nombre,"r") as archivo:
+ texto=archivo.read()
+
+tokens=nltk.word_tokenize(texto,"spanish")
+
+for token in tokens:
+ print(token)
+ 
+# Aquí, nuestra lista de tokens se llama "tokens"
+palabras_total=len(tokens)
+print(palabras_total)
